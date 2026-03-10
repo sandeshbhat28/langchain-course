2c19f4d5d8bf85e6ab711f9741bfd26eef1d04de
sandeshbhat28 <sandeshbhat28@gmail.com>
2026-03-09 21:26:27 -0700

    intro to search agents

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
-
 import os
+from re import search
 
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+#from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
+
+# Tavily Client used if we want to write our tool decorator function.
+# Else go with inbuilt TavilySearch
+#tavily = TavilyClient()
+
+# @tool
+# def search(query: str) -> dict:
+#     """tool that searches over internet
+#     Args:
+#         query :  query to search for
+#     Returns : search result """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
+
+
+#llm = ChatOllama(model="llama3.1:latest")
+#llm = ChatOpenAI(model="gpt-4o-mini-2024-07-18")
+
+llm = ChatOllama(model="mistral-large-3:675b-cloud")
+
+tools = [TavilySearch()]
+
+# Tavily Search used like above if we want Tavily to manage
+# else go with own tools capabilities
+#tools = [search]
+
+agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("hello from langchain course")
-
-    information = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2025; as of February 2026, Forbes estimates his net worth to be around US$852 billion.
-
-Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002."""
-
-    summary_template = ("Given the {information} about a person, I want to create"
-                        "1: short summary"
-                        "2. Two interesting facts about them")
-
-    summary_prompt_template = PromptTemplate(
-            input_variables = ["information"], template = summary_template)
-
-    # llm = ChatOpenAI(
-    #     model="gpt-4o-mini-2024-07-18",
-    #     temperature=None,
-    # )
-
-    llm = ChatOllama(
-        model="gemma3:4b",
-        temperature=None,
-    )
-
-    chain = summary_prompt_template | llm
-
-    response = chain.invoke(input = {"information":information})
-    print(response.content)
+    result = agent.invoke({"messages": [HumanMessage(
+        content="i want to search for 3 top tosca job openings in usa oregon(portland, beaverton, hillsboro)  on linkedin and list their details ?")]})
+    print(result["messages"][-1].content)
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
